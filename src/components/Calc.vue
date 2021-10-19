f176d2f897bd5e7b8f8f3d596bbbf8e9ad9ae5a3
toron2c <toron2c@yandex.ru>
2021-10-19 15:31:49 +0300

    lesson2

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,16 +1,36 @@
 <template>
   <div>
-    <input v-model.number="operand1">
-    <input v-model.number="operand2">
+    <input v-model.number="operand1" type="number">
+    <input v-model.number="operand2" type="number">
     <span> = </span>
     <span> {{ result }} </span>
     <div>
-      <button @click="plus">Плюс</button>
-      <button @click="minus">Минус</button>
-      <button @click="divide" :disabled="operand2 === 0">Деление</button>
-      <button @click="multiplication">Умножение</button>
-      <button @click="exponentiation">Возведение в степень</button>
-      <button @click="division" :disabled="operand2 === 0">Целочисленное деление</button>
+      <button v-for="item in operators"
+              :key="item.name"
+              @click="calcMethod(item.value)"
+              :title="item.name"
+              :disabled="item.value === '/' && operand2 === 0">
+        {{ item.name }}
+      </button>
+    </div>
+    <div class="keyboard">
+      <label>
+        <input type="checkbox" v-model="isKeyboard">
+        Включить клавиатуру
+      </label>
+      <div v-if="isKeyboard">
+        <button v-for="(item, idx) in buttons" :key="idx" @click="addCommand(item)">
+          {{ item }}
+        </button>
+        <div>
+          <label>
+            <input type="radio" v-model="selectedOperand" :value="'operand1'"> Операнд 1
+          </label>
+          <label>
+            <input type="radio" v-model="selectedOperand" :value="'operand2'"> Операнд 2
+          </label>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -23,9 +43,89 @@ export default {
       operand1: 0,
       operand2: 0,
       result: 0,
+      operators: [
+        {
+          name: 'Плюс',
+          value: '+',
+        },
+        {
+          name: 'Минус',
+          value: '-',
+        },
+        {
+          name: 'Деление',
+          value: '/',
+        },
+        {
+          name: 'Умножение',
+          value: '*',
+        },
+        {
+          name: 'Возведение',
+          value: '^'
+        },
+        {
+          name: 'Целочисленное деление',
+          value: '//',
+        }
+      ],
+      isKeyboard: false,
+      buttons: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '←'],
+      selectedOperand: 'operand1',
     }
   },
   methods: {
+    calcMethod(name) {
+      switch (name) {
+        case '+': {
+          this.plus();
+          break;
+        }
+        case '-': {
+          this.minus();
+          break;
+        }
+        case '/': {
+          this.divide();
+          break;
+        }
+        case '*': {
+          this.multiplication();
+          break;
+        }
+        case '^': {
+          this.exponentiation();
+          break;
+        }
+        case '//': {
+          this.division();
+          break;
+        }
+      }
+    },
+    addCommand(item) {
+      if (this.selectedOperand === 'operand1') {
+        if (item === '←') {
+          this.operand1 = this.operand1.toString()
+          this.operand1 = this.operand1.slice(0, -1);
+          this.operand1 = Number(this.operand1);
+        } else {
+          this.operand1 = this.operand1.toString();
+          this.operand1 = this.operand1 + item;
+          this.operand1 = Number(this.operand1);
+        }
+      } else {
+        if (item === '←') {
+          this.operand2 = this.operand2.toString()
+          this.operand2 = this.operand2.slice(0, -1);
+          this.operand2 = Number(this.operand2);
+        } else {
+          this.operand2 = this.operand2.toString();
+          this.operand2 = this.operand2 + item;
+          this.operand2 = Number(this.operand2);
+        }
+      }
+    },
     plus() {
       this.result = this.operand1 + this.operand2;
     },
@@ -52,7 +152,12 @@ export default {
 input {
   margin: 0 5px 0 0;
 }
+
 button {
+  margin: 5px;
+}
+
+.keyboard {
   margin: 5px;
 }
 </style>
